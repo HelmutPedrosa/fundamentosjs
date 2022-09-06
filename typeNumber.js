9bb2971c021ba8e0aadb74a348aa8e6d9d91736d
HelmutPedrosa <helmut.pedrosa@yahoo.com>
2022-09-05 22:25:58 -0400

    exemplos da aula 1

---
+++ typeNumber.js
@@ -0,0 +1,21 @@
+const meuNumero = 3;
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+
+const operacaoMatematica = primeiroNumero  + segundoNumero
+
+console.log(operacaoMatematica);
+
+//ponto flutuante
+
+const numeroPontoflutuante = 3.3;
+const pontoFlutuanteSemZero= .5;
+
+const novaOperacao = primeiroNumero+numeroPontoflutuante;
+
+console.log(novaOperacao)
+
+// NsN -> Not a Number (nao e um numero)
+
+
---
+++ typeString.js
@@ -0,0 +1,45 @@
+const texto1 = "Ola Mundo"
+const texto2 = 'Ola mundo'
+const senha = "minhaSenha123"
+const citacao = "Meu nome e:"
+const meuNome = "Helmut"
+
+console.log(texto1);
+
+console.log(texto2);
+
+console.log(senha);
+
+
+
+// Contatenacao (+)
+console.log (citacao + meuNome)
+
+
+//template string ou template literal
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+const copyritgh = '\u00A9'
+
+
+console.log(copyritgh)
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+console.log(cidade === input); // false
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); 
+
+
+console.log(senha.length) 
