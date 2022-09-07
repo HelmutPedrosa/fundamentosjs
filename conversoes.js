0f97440ca29c2a139b82da35cc8acce70eee9a59
HelmutPedrosa <helmut.pedrosa@yahoo.com>
2022-09-06 22:44:15 -0400

    https://github.com/HelmutPedrosa/codigos-var-e-conversoes.git

---
+++ conversoes.js
@@ -0,0 +1,27 @@
+//tipo de dado
+// booleanos
+
+//conversao implicita
+
+//conversao explicita
+
+const numero = 456
+const numeroString = Number("456");
+
+console.log(numero==numeroString);
+
+console.log(numero===numeroString);
+
+console.log(numero+numeroString);
+
+//Number()
+//String()
+
+// conversao implicita
+
+console.log(numero+Number(numeroString));
+
+// conversao explicita
+
+
+console.log(numero + numeroString);
